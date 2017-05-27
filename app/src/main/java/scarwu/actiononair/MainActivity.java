aeb3a4991f42cdf28436d4653dfb2b2c93cfd469
Scar Wu <xneriscool@gmail.com>
2017-05-27 20:55:02 +0800

    1. Add Facebook SDK 2. Add NFC Lib Proto

--- app/src/main/java/scarwu/actiononair/MainActivity.java
+++ app/src/main/java/scarwu/actiononair/MainActivity.java
@@ -3,11 +3,25 @@ package scarwu.actiononair;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import scarwu.actiononair.libaries.NFCReader;
+
 public class MainActivity extends AppCompatActivity {
+
+    private NFCReader nfcReader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
+        this.nfcReader = new NFCReader();
     }
+
+
 }
---
+++ app/src/main/java/scarwu/actiononair/libaries/NFCReader.java
@@ -0,0 +1,19 @@
+package scarwu.actiononair.libaries;
+
+public class NFCReader {
+    public NFCReader() {
+
+    }
+
+    public void start() {
+
+    }
+
+    public void stop() {
+
+    }
+
+    public void getData() {
+
+    }
+}
