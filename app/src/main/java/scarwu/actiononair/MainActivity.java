a8905bc046eaaa4d60bba6aea889ce76671e24cf
Scar Wu <xneriscool@gmail.com>
2017-05-28 17:08:10 +0800

    1. Now can read nfc tag

--- app/src/main/java/scarwu/actiononair/MainActivity.java
+++ app/src/main/java/scarwu/actiononair/MainActivity.java
@@ -12,15 +12,31 @@ package scarwu.actiononair;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.app.Fragment;
-import android.widget.PopupWindow;
-
+import android.app.PendingIntent;
 import android.view.View;
+import android.util.Log;
+
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.tech.NfcA;
+import android.nfc.tech.NfcB;
+import android.nfc.tech.NfcF;
+import android.nfc.tech.NfcV;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NdefFormatable;
+import android.nfc.tech.MifareClassic;
+import android.nfc.tech.MifareUltralight;
+import android.nfc.tech.IsoDep;
 
 // Custom Libs
 import scarwu.actiononair.ControlPanelActivity;
@@ -46,6 +62,12 @@ public class MainActivity extends AppCompatActivity {
     private boolean isFBAuth = false;
     private boolean isGoogleAuth = false;
 
+    // NFC
+    private NfcAdapter nfcAdapter;
+    private PendingIntent nfcPendingIntent;
+    private IntentFilter[] nfcFilters;
+    private String[][] nfcTechLists;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,9 +75,81 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Initailize View Widgets
-        initSNSFacebookWidgets();;
+        initSNSFacebookWidgets();
         initSNSGoogleWidgets();
         initCameraWidgets();
+
+        // NFC
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        nfcPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
+
+        try {
+            ndef.addDataType("*/*");
+        } catch (IntentFilter.MalformedMimeTypeException e) {
+            throw new RuntimeException("fail", e);
+        }
+
+        IntentFilter[] nfcFilters = new IntentFilter[] {
+            ndef
+        };
+
+        // Setup a tech list for all NfcF tags
+        String[][] nfcTechLists = new String[][] {
+            new String[] {
+                MifareClassic.class.getName()
+            }
+        };
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        nfcAdapter.disableForegroundDispatch(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        nfcAdapter.enableForegroundDispatch(this, nfcPendingIntent, nfcFilters, nfcTechLists);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        Log.i("NFC", "Intent: " + intent);
+
+        // Get Action from Intent
+        String action = intent.getAction();
+
+        Log.i("NFC", "Action: " + action);
+
+        //  3) Get an instance of the TAG from the NfcAdapter
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            Log.i("NFC", "Action: " + NfcAdapter.ACTION_NDEF_DISCOVERED);
+
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+
+            byte[] id = tag.getId();
+
+            try {
+                Log.i("NFC", "ID: " + getHexString(id));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static String getHexString(byte[] b) throws Exception {
+        String result = "";
+
+        for (int i=0; i < b.length; i++) {
+            result += Integer.toString( ( b[i] & 0xff ) + 0x100, 16).substring(1);
+        }
+
+        return result;
     }
 
     /**
